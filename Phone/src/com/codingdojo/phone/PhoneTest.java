83e75814d7b5d4440d17e06900e783911e2e798a
npulliam <nathanpulliam@gmail.com>
2021-04-07 13:23:03 -0700

    Phone

---
+++ Phone/src/com/codingdojo/phone/PhoneTest.java
@@ -0,0 +1,19 @@
+package com.codingdojo.phone;
+
+public class PhoneTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Galaxy s9 = new Galaxy("S9", 99, "Verizon", "Ring Ring Ring!");
+		IPhone iPhoneTen = new IPhone("X", 100, "AT&T", "Zing");
+		
+		s9.displayInfo();
+		System.out.println(s9.ring());
+		System.out.println(s9.unlock());
+		
+		iPhoneTen.displayInfo();
+		System.out.println(iPhoneTen.ring());
+		System.out.println(iPhoneTen.unlock());
+	}
+
+}
